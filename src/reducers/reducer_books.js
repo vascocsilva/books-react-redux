c5497e8a7f1ad48b8f2750ed5325963bbaf94369
Vasco Silva <vasco.s.rebolo@gmail.com>
2016-10-06 15:44:36 +0100

    Add first smart component (container), add eslintrc

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'The Great Gatsby' },
+    { title: 'Freedom' },
+    { title: 'All the light we cannot see' },
+    { title: '1984' },
+    { title: 'Brave New World' }
+  ];
+}
