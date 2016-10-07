26bf4c9ee3fbe096d2b146a5bb8d1d2b792ee9ca
Vasco Silva <vasco.s.rebolo@gmail.com>
2016-10-07 10:11:21 +0100

    Add all components, finish simple redux implementation

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'The Great Gatsby', pages: 101 },
+    { title: 'Freedom', pages: 345 },
+    { title: 'All the light we cannot see', pages: 312 },
+    { title: '1984', pages: 264 },
+    { title: 'Brave New World', pages: 300 }
+  ];
+}
